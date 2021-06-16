e90f65a118ec42951da244dc8532980648a773d9
Dmitry Momot <mail@dmomot.com>
2021-06-16 07:34:15 +0300

    fixed duplicate lang key 'txt_verification'

--- lib/translation/en.dart
+++ lib/translation/en.dart
@@ -57,7 +57,6 @@ const Map<String, String> en = {
   'txt_claim_rewards': 'Claim rewards',
   'txt_congrats': 'Congratulations',
   'txt_congrats_msg': 'You have won, and been awarded',
-  'txt_verification': 'Verification',
   'txt_verification_text':
       'Please enter the verification code we’ve sent to your email address or use the link from your inbox'
 };
