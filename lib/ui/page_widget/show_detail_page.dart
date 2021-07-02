77d61af451d4380030f787bcd7e3b2087977f22a
Michael Yemets <mgyemets@gmail.com>
2021-07-02 19:33:21 +0300

    Move 'ShowDetail' screen to relative values Remove bottom sheet expand Remove trailer Add shadows to left & right images edges

--- lib/ui/page_widget/show_detail_page.dart
+++ lib/ui/page_widget/show_detail_page.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:satorio/controller/show_detail_controller.dart';
 import 'package:satorio/domain/entities/show.dart';
+import 'package:satorio/ui/theme/light_theme.dart';
 import 'package:satorio/ui/theme/sator_color.dart';
+import 'package:satorio/ui/theme/text_theme.dart';
 import 'package:satorio/ui/widget/elevated_gradient_button.dart';
 
 class ShowDetailPage extends GetView<ShowDetailController> {
-  final RxBool expanded = false.obs;
-
-  final bottomSheetMinHeight = 342;
+  final bottomSheetMinHeight = 300 * coefficient;
 
   ShowDetailPage(Show show) : super() {
     controller.loadShowDetail(show);
@@ -36,6 +35,34 @@ class ShowDetailPage extends GetView<ShowDetailController> {
             fit: BoxFit.cover,
             errorBuilder: (context, error, stackTrace) => Container(
               color: SatorioColor.darkAccent,
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            height: Get.height,
+            width: 100 * coefficient,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Colors.black.withOpacity(0.5),
+                Colors.black.withOpacity(0)
+              ]),
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: Container(
+            height: Get.height,
+            width: 100 * coefficient,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Colors.black.withOpacity(0),
+                Colors.black.withOpacity(0.5)
+              ]),
+              borderRadius: BorderRadius.circular(8),
             ),
           ),
         ),
@@ -70,55 +97,55 @@ class ShowDetailPage extends GetView<ShowDetailController> {
               children: [
                 Icon(
                   Icons.favorite_outline_rounded,
-                  size: 22,
+                  size: 22.0 * coefficient,
                   color: Colors.white,
                 ),
                 SizedBox(
-                  height: 6,
+                  height: 6.0 * coefficient,
                 ),
                 Text(
                   '32',
-                  style: TextStyle(
+                  style: textTheme.subtitle2!.copyWith(
                     color: Colors.white,
-                    fontSize: 15.0,
+                    fontSize: 15.0 * coefficient,
                     fontWeight: FontWeight.w500,
                   ),
                 ),
                 SizedBox(
-                  height: 23,
+                  height: 23.0 * coefficient,
                 ),
                 Icon(
                   Icons.photo_outlined,
-                  size: 22,
+                  size: 22.0 * coefficient,
                   color: Colors.white,
                 ),
                 SizedBox(
-                  height: 6,
+                  height: 6.0 * coefficient,
                 ),
                 Text(
                   '325',
-                  style: TextStyle(
+                  style: textTheme.subtitle2!.copyWith(
                     color: Colors.white,
-                    fontSize: 15.0,
+                    fontSize: 15.0 * coefficient,
                     fontWeight: FontWeight.w500,
                   ),
                 ),
                 SizedBox(
-                  height: 23,
+                  height: 23.0 * coefficient,
                 ),
                 Icon(
                   Icons.mode_comment_outlined,
-                  size: 22,
+                  size: 22.0 * coefficient,
                   color: Colors.white,
                 ),
                 SizedBox(
-                  height: 6,
+                  height: 6.0 * coefficient,
                 ),
                 Text(
                   '35',
-                  style: TextStyle(
+                  style: textTheme.subtitle2!.copyWith(
                     color: Colors.white,
-                    fontSize: 15.0,
+                    fontSize: 15.0 * coefficient,
                     fontWeight: FontWeight.w500,
                   ),
                 ),
@@ -131,168 +158,112 @@ class ShowDetailPage extends GetView<ShowDetailController> {
   }
 
   Widget _bottomSheetContent() {
-    final double minSize = bottomSheetMinHeight / Get.height;
-    final double maxSize =
-        (Get.height - Get.mediaQuery.padding.top - 1) / Get.height;
-
-    return NotificationListener<DraggableScrollableNotification>(
-      onNotification: (notification) {
-        expanded.value = notification.extent == notification.maxExtent;
-        return true;
-      },
-      child: DraggableScrollableSheet(
-        initialChildSize: minSize,
-        minChildSize: minSize,
-        maxChildSize: maxSize,
-        expand: false,
-        builder: (context, scrollController) => LayoutBuilder(
-          builder: (context, constraints) => SingleChildScrollView(
-            controller: scrollController,
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                  minWidth: constraints.maxWidth,
-                  minHeight: constraints.maxHeight),
-              child: IntrinsicHeight(
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                      top: 32, left: 20, right: 20, bottom: 32),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Obx(
-                        () => Text(
-                          controller.showDetailRx.value?.title ?? '',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 4,
-                      ),
-                      Expanded(
-                        child: Obx(
-                          () => Text(
-                            _descr,
-                            overflow:
-                                expanded.value ? null : TextOverflow.ellipsis,
-                            maxLines: expanded.value ? null : 4,
-                            textAlign: TextAlign.justify,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 18,
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.play_circle_outline_rounded,
-                            size: 20,
-                            color: SatorioColor.darkAccent.withOpacity(0.5),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Expanded(
-                            child: Text(
-                              'txt_watch_trailer'.tr,
-                              style: TextStyle(
-                                color: SatorioColor.darkAccent.withOpacity(0.5),
-                                fontSize: 15.0,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 22,
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            'txt_challenges_available'.tr,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 4,
-                          ),
-                          Expanded(
-                            child: Text(
-                              '12',
-                              textAlign: TextAlign.end,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15.0,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            'txt_rewards_earned'.tr,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 4,
-                          ),
-                          Expanded(
-                            child: Text(
-                              12.55555555.toStringAsFixed(2),
-                              textAlign: TextAlign.end,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15.0,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      SizedBox(
-                        height: 48,
-                        child: ElevatedGradientButton(
-                          text: 'txt_episodes'.tr,
-                          onPressed: () {
-                            controller.toEpisodes();
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
+    return Container(
+      // height: bottomSheetMinHeight,
+      child: Padding(
+        padding:
+            const EdgeInsets.only(top: 32, left: 20, right: 20, bottom: 32),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Obx(
+              () => Text(
+                controller.showDetailRx.value?.title ?? '',
+                style: textTheme.headline3!.copyWith(
+                  color: Colors.black,
+                  fontSize: 24.0 * coefficient,
+                  fontWeight: FontWeight.w700,
                 ),
               ),
             ),
-          ),
+            SizedBox(
+              height: 4.0 * coefficient,
+            ),
+            Text(
+              _descr,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 4,
+              textAlign: TextAlign.justify,
+              style: textTheme.bodyText2!.copyWith(
+                color: Colors.black,
+                fontSize: 15.0 * coefficient,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            SizedBox(
+              height: 20.0 * coefficient,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'txt_challenges_available'.tr,
+                  style: textTheme.bodyText2!.copyWith(
+                    color: Colors.black,
+                    fontSize: 15.0 * coefficient,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(
+                  width: 4.0 * coefficient,
+                ),
+                Expanded(
+                  child: Text(
+                    '12',
+                    textAlign: TextAlign.end,
+                    style: textTheme.bodyText2!.copyWith(
+                      color: Colors.black,
+                      fontSize: 15.0 * coefficient,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 8.0 * coefficient,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'txt_rewards_earned'.tr,
+                  style: textTheme.bodyText2!.copyWith(
+                    color: Colors.black,
+                    fontSize: 15.0 * coefficient,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(
+                  width: 4.0 * coefficient,
+                ),
+                Expanded(
+                  child: Text(
+                    12.55555555.toStringAsFixed(2),
+                    textAlign: TextAlign.end,
+                    style: textTheme.bodyText2!.copyWith(
+                      color: Colors.black,
+                      fontSize: 15.0 * coefficient,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20.0 * coefficient,
+            ),
+            SizedBox(
+              height: 48.0,
+              child: ElevatedGradientButton(
+                text: 'txt_episodes'.tr,
+                onPressed: () {
+                  controller.toEpisodes();
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
