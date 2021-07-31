ea0ec9091e9b16a5d2dfd2abca6beadf5b8200ac
Michael Yemets <mgyemets@gmail.com>
2021-07-31 23:50:56 +0300

    Implement 'Scan Qr' action on 'Send' screen

--- lib/controller/qr_scanner_controller.dart
+++ lib/controller/qr_scanner_controller.dart
@@ -3,8 +3,10 @@ import 'dart:convert';
 import 'package:get/get.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:satorio/binding/qr_result_show_binding.dart';
+import 'package:satorio/binding/wallet_send_binding.dart';
 import 'package:satorio/controller/mixin/back_to_main_mixin.dart';
 import 'package:satorio/controller/qr_result_show_controller.dart';
+import 'package:satorio/controller/wallet_send_controller.dart';
 import 'package:satorio/data/model/qr/qr_data_factory.dart';
 import 'package:satorio/domain/entities/qr/qr_data.dart';
 import 'package:satorio/domain/entities/qr/qr_payload_show.dart';
@@ -12,23 +14,24 @@ import 'package:satorio/domain/entities/qr/qr_payload_wallet_send.dart';
 import 'package:satorio/domain/entities/qr_show.dart';
 import 'package:satorio/domain/repositories/sator_repository.dart';
 import 'package:satorio/ui/page_widget/qr_result_show_page.dart';
+import 'package:satorio/ui/page_widget/wallet_send_page.dart';
 
 class QrScannerController extends GetxController with BackToMainMixin {
   final SatorioRepository _satorioRepository = Get.find();
+
+  late final RxBool isResultBackRx;
   Barcode? result;
 
-  void back() {
-    Get.back();
+  QrScannerController() {
+    QrScannerArgument argument = Get.arguments as QrScannerArgument;
+    isResultBackRx = argument.isResultBack.obs;
   }
 
-  void _loadShow(QrShow qrShow) {
-    _satorioRepository.loadShow(qrShow.showId).then((show) {
-      Get.off(
-        () => QrResultShowPage(),
-        binding: QrResultShowBinding(),
-        arguments: QrResultShowArgument(show, qrShow),
-      );
-    });
+  void back() {
+    if (isResultBackRx.value)
+      Get.back(result: null);
+    else
+      Get.back();
   }
 
   void startScan(QRViewController qrController) {
@@ -40,6 +43,7 @@ class QrScannerController extends GetxController with BackToMainMixin {
   }
 
   void _initScanner(String scanData) {
+    print(scanData);
     QrData data = QrDataModelFactory.createQrData(jsonDecode(scanData));
     switch (data.type) {
       case QrType.show:
@@ -53,12 +57,33 @@ class QrScannerController extends GetxController with BackToMainMixin {
 
   void _handleShowData(String code) {
     _satorioRepository.getShowEpisodeByQR(code).then((qrResult) {
-      QrShow show = qrResult;
-      _loadShow(show);
+      QrShow qrShow = qrResult;
+
+      _satorioRepository.loadShow(qrShow.showId).then((show) {
+        Get.off(
+          () => QrResultShowPage(),
+          binding: QrResultShowBinding(),
+          arguments: QrResultShowArgument(show, qrShow),
+        );
+      });
     });
   }
 
   void _handleWalletData(String walletAddress) {
-    // TODO
+    if (isResultBackRx.value) {
+      Get.back(result: walletAddress);
+    } else {
+      Get.off(
+        () => WalletSendPage(),
+        binding: WalletSendBinding(),
+        arguments: WalletSendArgument(null, walletAddress),
+      );
+    }
   }
+}
+
+class QrScannerArgument {
+  final bool isResultBack;
+
+  const QrScannerArgument(this.isResultBack);
 }
--- lib/controller/wallet_send_controller.dart
+++ lib/controller/wallet_send_controller.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:satorio/binding/qr_scanner_binding.dart';
 import 'package:satorio/binding/transaction_preview_binding.dart';
+import 'package:satorio/controller/qr_scanner_controller.dart';
 import 'package:satorio/domain/entities/wallet_detail.dart';
 import 'package:satorio/ui/bottom_sheet_widget/transacting_tips_bottom_sheet.dart';
+import 'package:satorio/ui/page_widget/qr_scanner_page.dart';
 import 'package:satorio/ui/page_widget/transaction_preview_page.dart';
 
 class WalletSendController extends GetxController {
@@ -12,12 +15,15 @@ class WalletSendController extends GetxController {
 
   late final Rx<WalletDetail?> fromWalletDetailRx;
 
-  final RxBool toAddressVisibility = false.obs;
+  late final RxBool toAddressVisibility;
 
   WalletSendController() {
     WalletSendArgument argument = Get.arguments as WalletSendArgument;
     toAddressController = TextEditingController(text: argument.toAddress);
     fromWalletDetailRx = Rx(argument.fromWalletDetail);
+
+    toAddressVisibility =
+        (argument.toAddress != null && argument.toAddress!.isNotEmpty).obs;
   }
 
   void back() {
@@ -49,8 +55,23 @@ class WalletSendController extends GetxController {
     toAddressVisibility.value = true;
   }
 
-  void scanQr() {
-    // TODO
+  void scanQr() async {
+    String? result = await Get.to(
+      () => QrScannerPage(),
+      binding: QrScannerBinding(),
+      arguments: QrScannerArgument(true),
+    );
+
+    if (result != null && result.isNotEmpty) {
+      toAddressController.value = TextEditingValue(
+        text: result,
+        selection: TextSelection.fromPosition(
+          TextPosition(offset: result.length),
+        ),
+      );
+
+      toAddressVisibility.value = true;
+    }
   }
 }
 
