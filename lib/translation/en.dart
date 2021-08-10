f131a93976d9deca3de39cdf10e6332e56cb9faf
Michael Yemets <mgyemets@gmail.com>
2021-08-10 14:54:37 +0300

    Fix after merge

--- lib/translation/en.dart
+++ lib/translation/en.dart
@@ -114,7 +114,7 @@ const Map<String, String> en = {
   'txt_preview': 'Preview',
   'txt_transacting_tips': 'Transacting Tips',
   'txt_transacting_tips_description':
-  'Before you finalize your transaction, keep in mind a couple of tips:',
+      'Before you finalize your transaction, keep in mind a couple of tips:',
   'txt_more_info_transacting_tips': 'For more info about transactions tap ',
   'txt_here': 'here',
   'txt_i_understand': 'I understand',
@@ -138,7 +138,6 @@ const Map<String, String> en = {
   'txt_coming_soon': 'Coming soon...',
   'txt_choose_your_destination_wallet': 'Choose your destination wallet',
   'txt_choose_your_source_wallet': 'Choose your source wallet',
-  'txt_success': 'Success',
   'txt_transaction_sent': 'Transaction was sent',
   'txt_transaction_not_sent': 'Transaction was not sent',
   'txt_cool': 'Cool',
