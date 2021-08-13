f8933ba436e5754aa35f73e8ea634bab56834d63
Mike Bortsov <mike.bortsov@gmail.com>
2021-08-13 13:46:28 +0300

    global widget added

--- lib/ui/page_widget/home_page.dart
+++ lib/ui/page_widget/home_page.dart
@@ -10,6 +10,7 @@ import 'package:satorio/domain/entities/show.dart';
 import 'package:satorio/ui/theme/light_theme.dart';
 import 'package:satorio/ui/theme/sator_color.dart';
 import 'package:satorio/ui/theme/text_theme.dart';
+import 'package:satorio/ui/widget/title_button.dart';
 import 'package:satorio/util/avatar_list.dart';
 
 class HomePage extends GetView<HomeController> {
@@ -18,7 +19,7 @@ class HomePage extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: SatorioColor.darkAccent,
+      backgroundColor: Colors.transparent,
       extendBodyBehindAppBar: true,
       body: RefreshIndicator(
         color: SatorioColor.brand,
@@ -39,7 +40,8 @@ class HomePage extends GetView<HomeController> {
                       fit: BoxFit.cover,
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(left: 20, top: 76, right: 20),
+                      padding:
+                          const EdgeInsets.only(left: 20, top: 76, right: 20),
                       child: Align(
                         alignment: Alignment.topLeft,
                         child: InkWell(
@@ -64,21 +66,23 @@ class HomePage extends GetView<HomeController> {
                                         color: SatorioColor.casablanca,
                                       ),
                                       child: Center(
-                                        child: ClipOval(
-                                          child: SvgPicture.asset(
-                                            avatars[avatarIndex],
-                                            width: 50,
-                                            height: 50,
-                                            fit: BoxFit.fitWidth,
-                                          ),
+                                        child: SvgPicture.asset(
+                                          avatars[avatarIndex],
+                                          width: 50,
+                                          height: 50,
+                                          fit: BoxFit.fitWidth,
                                         ),
                                       ),
                                     ),
                                   ),
-                                  SizedBox(width: 14,),
+                                  SizedBox(
+                                    width: 14,
+                                  ),
                                   Obx(
                                     () => Text(
-                                      controller.profileRx.value?.displayedName ?? '',
+                                      controller
+                                              .profileRx.value?.displayedName ??
+                                          '',
                                       style: textTheme.headline1!.copyWith(
                                           color: SatorioColor.darkAccent,
                                           fontSize: 14.0 * coefficient,
@@ -88,17 +92,17 @@ class HomePage extends GetView<HomeController> {
                                 ],
                               ),
                               Obx(
-                                    () => Column(
+                                () => Column(
                                   crossAxisAlignment: CrossAxisAlignment.end,
                                   children: [
                                     Text(
                                       controller.walletRx.value.length > 0
                                           ? controller
-                                          .walletRx.value[0].displayedValue
+                                              .walletRx.value[0].displayedValue
                                           : '',
                                       style: textTheme.bodyText1!.copyWith(
                                         color: SatorioColor.darkAccent,
-                                        fontSize: 24.0 * coefficient,
+                                        fontSize: 18.0 * coefficient,
                                         fontWeight: FontWeight.w700,
                                       ),
                                     ),
@@ -137,15 +141,8 @@ class HomePage extends GetView<HomeController> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
-          padding: const EdgeInsets.only(top: 32, left: 20, right: 20),
-          child: Text(
-            'Highest Rewarding',
-            style: textTheme.headline3!.copyWith(
-              color: SatorioColor.textBlack,
-              fontSize: 24.0 * coefficient,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
+          padding: const EdgeInsets.only(top: 24, left: 20, right: 20),
+          child: TitleWithButton(textCode: 'Highest Rewarding', onTap: () {}),
         ),
         Container(
           margin: const EdgeInsets.only(top: 16),
@@ -167,14 +164,7 @@ class HomePage extends GetView<HomeController> {
         ),
         Padding(
           padding: const EdgeInsets.only(top: 24, left: 20, right: 20),
-          child: Text(
-            'Most Socializing',
-            style: textTheme.headline3!.copyWith(
-              color: SatorioColor.textBlack,
-              fontSize: 24.0 * coefficient,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
+          child: TitleWithButton(textCode: 'Most Socializing', onTap: () {}),
         ),
         Container(
           margin: const EdgeInsets.only(top: 16),
@@ -196,14 +186,7 @@ class HomePage extends GetView<HomeController> {
         ),
         Padding(
           padding: const EdgeInsets.only(top: 24, left: 20, right: 20),
-          child: Text(
-            'Newest Added',
-            style: textTheme.headline3!.copyWith(
-              color: SatorioColor.textBlack,
-              fontSize: 24.0 * coefficient,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
+          child: TitleWithButton(textCode: 'Newest Added', onTap: () {}),
         ),
         Container(
           margin: const EdgeInsets.only(top: 16),
