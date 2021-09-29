ea2925288156662c702b39fc9526e2f327c467e0
Michael Yemets <mgyemets@gmail.com>
2021-09-29 21:20:30 +0300

    Update texts

--- lib/controller/mixin/non_working_feature_mixin.dart
+++ lib/controller/mixin/non_working_feature_mixin.dart
@@ -5,7 +5,7 @@ mixin NonWorkingFeatureMixin {
   toNonWorkingFeatureDialog() {
     Get.dialog(
       DefaultDialog(
-        'txt_oops'.tr,
+        'txt_coming_soon'.tr,
         'txt_non_working_feature'.tr,
         'txt_ok'.tr,
       ),
