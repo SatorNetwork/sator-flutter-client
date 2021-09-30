13e967a07725e555a498f115c2f8718fdf15d296
MichaelBoostyLabs <84083729+MichaelBoostyLabs@users.noreply.github.com>
2021-09-30 20:50:09 +0300

    Fix wrong avatar on the quiz lobby screen (#119)
    Co-authored-by: Michael Yemets <mgyemets@gmail.com>

--- lib/domain/entities/payload/payload_user.dart
+++ lib/domain/entities/payload/payload_user.dart
@@ -3,9 +3,11 @@ import 'package:satorio/domain/entities/payload/payload.dart';
 class PayloadUser extends Payload {
   final String userId;
   final String username;
+  final String avatar;
 
   const PayloadUser(
     this.userId,
     this.username,
+    this.avatar,
   );
 }
