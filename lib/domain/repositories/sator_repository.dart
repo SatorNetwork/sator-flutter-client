62230efb0254332e8230be6b6d8aae02fc1e9012
Mikhail Bortsov <38129113+mbortsov@users.noreply.github.com>
2021-10-26 15:28:54 +0300

    SAT-501 Settings (#135)
    * settings page added
    
    * settings page UI finished
    
    * settings about page added
    
    * update email request added
    
    * update username request added
    
    * todo eddited
    
    * refactored

--- lib/domain/repositories/sator_repository.dart
+++ lib/domain/repositories/sator_repository.dart
@@ -24,7 +24,13 @@ abstract class SatorioRepository {
 
   Future<bool> signUp(String email, String password, String username);
 
+  Future<bool> requestUpdateEmail(String email);
+
+  Future<bool> updateUsername(String username);
+
   Future<bool> verifyAccount(String code);
+
+  Future<bool> verifyUpdateEmail(String email, String code);
 
   Future<bool> isVerified();
 
