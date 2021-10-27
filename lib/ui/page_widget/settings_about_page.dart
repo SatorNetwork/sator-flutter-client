411af8321817e46ac739e57bdc4195aac309dad3
Mikhail Bortsov <38129113+mbortsov@users.noreply.github.com>
2021-10-27 11:18:48 +0300

    SAT-509 Link privacy policy (#138)
    * link to privacy policy added
    
    * refactored

--- lib/ui/page_widget/settings_about_page.dart
+++ lib/ui/page_widget/settings_about_page.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import 'package:satorio/controller/settings_about_controller.dart';
 import 'package:satorio/ui/theme/light_theme.dart';
 import 'package:satorio/ui/theme/text_theme.dart';
+import 'package:satorio/util/links.dart';
 
 class SettingsAboutPage extends GetView<SettingsAboutController> {
   @override
@@ -59,8 +60,8 @@ class SettingsAboutPage extends GetView<SettingsAboutController> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        _aboutButton('txt_policy'.tr, () => controller.toNonWorkingDialog()),
-        _aboutButton('txt_terms'.tr, () => controller.toTermsOfUse()),
+        _aboutButton('txt_policy'.tr, () => controller.toWebPage(linkPrivacyPolicy)),
+        _aboutButton('txt_terms'.tr, () => controller.toWebPage(linkTermsOfUse)),
       ],
     );
   }
