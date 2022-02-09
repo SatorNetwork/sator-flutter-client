0420e3a931129045ab6e0749136922041ffa9445
mbortsov <mike.bortsov@gmail.com>
2022-02-09 15:06:10 +0200

    change email bug fixed

--- lib/controller/settings_change_info_controller.dart
+++ lib/controller/settings_change_info_controller.dart
@@ -75,6 +75,7 @@ class SettingsChangeInfoController extends GetxController
     }).then((value) {
       _satorioRepository.requestUpdateEmail(emailRx.value).then((isSuccess) {
         if (isSuccess) {
+          Get.back();
           Get.to(
             () => EmailVerificationPage(),
             binding: EmailVerificationBinding(),
