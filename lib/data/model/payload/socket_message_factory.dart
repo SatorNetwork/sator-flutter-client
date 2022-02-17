fbb5dd57d4bccde08607a73921623acd9f33b007
Michael Yemets <mgyemets@gmail.com>
2022-02-17 19:10:22 +0200

    add ttl properties to models

--- lib/data/model/payload/socket_message_factory.dart
+++ lib/data/model/payload/socket_message_factory.dart
@@ -8,154 +8,262 @@ import 'package:satorio/data/model/payload/payload_time_out_model.dart';
 import 'package:satorio/data/model/payload/payload_user_model.dart';
 import 'package:satorio/data/model/to_json_interface.dart';
 import 'package:satorio/domain/entities/payload/socket_message.dart';
+import 'package:satorio/util/extension.dart';
 
 class SocketMessagePlayerConnectedModel extends SocketMessagePlayerConnected
     implements ToJsonInterface {
-  SocketMessagePlayerConnectedModel(PayloadUserModel payload) : super(payload);
+  SocketMessagePlayerConnectedModel(
+    PayloadUserModel payload,
+    DateTime? date,
+    int ttl,
+  ) : super(
+          payload,
+          date,
+          ttl,
+        );
 
   @override
   Map toJson() => {
         'type': type,
         'payload': (payload as ToJsonInterface).toJson(),
+        'date': date?.toIso8601String() ?? '',
+        'ttl': ttl,
       };
 }
 
 class SocketMessagePlayerDisconnectedModel
     extends SocketMessagePlayerDisconnected implements ToJsonInterface {
-  SocketMessagePlayerDisconnectedModel(PayloadUserModel payload)
-      : super(payload);
+  SocketMessagePlayerDisconnectedModel(
+    PayloadUserModel payload,
+    DateTime? date,
+    int ttl,
+  ) : super(
+          payload,
+          date,
+          ttl,
+        );
 
   @override
   Map toJson() => {
         'type': type,
         'payload': (payload as ToJsonInterface).toJson(),
+        'date': date?.toIso8601String() ?? '',
+        'ttl': ttl,
       };
 }
 
 class SocketMessageCountdownModel extends SocketMessageCountdown
     implements ToJsonInterface {
-  SocketMessageCountdownModel(PayloadCountdownModel payload) : super(payload);
+  SocketMessageCountdownModel(
+    PayloadCountdownModel payload,
+    DateTime? date,
+    int ttl,
+  ) : super(
+          payload,
+          date,
+          ttl,
+        );
 
   @override
   Map toJson() => {
         'type': type,
         'payload': (payload as ToJsonInterface).toJson(),
+        'date': date?.toIso8601String() ?? '',
+        'ttl': ttl,
       };
 }
 
 class SocketMessageQuestionModel extends SocketMessageQuestion
     implements ToJsonInterface {
-  SocketMessageQuestionModel(PayloadQuestionModel payload) : super(payload);
+  SocketMessageQuestionModel(
+    PayloadQuestionModel payload,
+    DateTime? date,
+    int ttl,
+  ) : super(
+          payload,
+          date,
+          ttl,
+        );
 
   @override
   Map toJson() => {
         'type': type,
         'payload': (payload as ToJsonInterface).toJson(),
+        'date': date?.toIso8601String() ?? '',
+        'ttl': ttl,
       };
 }
 
 class SocketMessageQuestionResultModel extends SocketMessageQuestionResult
     implements ToJsonInterface {
-  SocketMessageQuestionResultModel(PayloadQuestionResultModel payload)
-      : super(payload);
+  SocketMessageQuestionResultModel(
+    PayloadQuestionResultModel payload,
+    DateTime? date,
+    int ttl,
+  ) : super(
+          payload,
+          date,
+          ttl,
+        );
 
   @override
   Map toJson() => {
         'type': type,
         'payload': (payload as ToJsonInterface).toJson(),
+        'date': date?.toIso8601String() ?? '',
+        'ttl': ttl,
       };
 }
 
 class SocketMessageChallengeResultModel extends SocketMessageChallengeResult
     implements ToJsonInterface {
-  SocketMessageChallengeResultModel(PayloadChallengeResultModel payload)
-      : super(payload);
+  SocketMessageChallengeResultModel(
+    PayloadChallengeResultModel payload,
+    DateTime? date,
+    int ttl,
+  ) : super(
+          payload,
+          date,
+          ttl,
+        );
 
   @override
   Map toJson() => {
         'type': type,
         'payload': (payload as ToJsonInterface).toJson(),
+        'date': date?.toIso8601String() ?? '',
+        'ttl': ttl,
       };
 }
 
 class SocketMessageAnswerModel extends SocketMessageAnswer
     implements ToJsonInterface {
-  SocketMessageAnswerModel(PayloadAnswerModel payload) : super(payload);
+  SocketMessageAnswerModel(
+    PayloadAnswerModel payload,
+    DateTime? date,
+    int ttl,
+  ) : super(
+          payload,
+          date,
+          ttl,
+        );
 
   @override
   Map toJson() => {
         'type': type,
         'payload': (payload as ToJsonInterface).toJson(),
+        'date': date?.toIso8601String() ?? '',
+        'ttl': ttl,
       };
 }
 
 class SocketMessageTimeOutModel extends SocketMessageTimeOut
     implements ToJsonInterface {
-  SocketMessageTimeOutModel(PayloadTimeOutModel payload) : super(payload);
+  SocketMessageTimeOutModel(
+    PayloadTimeOutModel payload,
+    DateTime? date,
+    int ttl,
+  ) : super(
+          payload,
+          date,
+          ttl,
+        );
 
   @override
   Map toJson() => {
         'type': type,
         'payload': (payload as ToJsonInterface).toJson(),
+        'date': date?.toIso8601String() ?? '',
+        'ttl': ttl,
       };
 }
 
 class SocketMessagePlayerIsActiveModel extends SocketMessagePlayerIsActive
     implements ToJsonInterface {
-  SocketMessagePlayerIsActiveModel(PayloadEmptyModel payload) : super(payload);
+  SocketMessagePlayerIsActiveModel(
+    PayloadEmptyModel payload,
+    DateTime? date,
+    int ttl,
+  ) : super(
+          payload,
+          date,
+          ttl,
+        );
 
   @override
   Map toJson() => {
         'type': type,
         'payload': (payload as ToJsonInterface).toJson(),
+        'date': date?.toIso8601String() ?? '',
+        'ttl': ttl,
       };
 }
 
 class SocketMessageModelFactory {
   static SocketMessage createSocketMessage(Map json) {
-    String type = json['type'] == null
+    final String type = json['type'] == null
         ? ''
         : (json['type'] is int
             ? Type.fromInt(json['type'] as int)
             : json['type'] as String);
-    Map payloadJson = json['payload'];
+    final DateTime? date = json.tryParseValueAsDateTime('date');
+    final int ttl = json.parseValueAsInt('ttl');
+    final Map payloadJson = json['payload'];
 
     switch (type) {
       case Type.player_connected:
         return SocketMessagePlayerConnectedModel(
           PayloadUserModel.fromJson(payloadJson),
+          date,
+          ttl,
         );
       case Type.player_disconnected:
         return SocketMessagePlayerDisconnectedModel(
           PayloadUserModel.fromJson(payloadJson),
+          date,
+          ttl,
         );
       case Type.countdown:
         return SocketMessageCountdownModel(
           PayloadCountdownModel.fromJson(payloadJson),
+          date,
+          ttl,
         );
       case Type.question:
         return SocketMessageQuestionModel(
           PayloadQuestionModel.fromJson(payloadJson),
+          date,
+          ttl,
         );
       case Type.question_result:
         return SocketMessageQuestionResultModel(
           PayloadQuestionResultModel.fromJson(payloadJson),
+          date,
+          ttl,
         );
       case Type.challenge_result:
         return SocketMessageChallengeResultModel(
           PayloadChallengeResultModel.fromJson(payloadJson),
+          date,
+          ttl,
         );
       case Type.answer:
         return SocketMessageAnswerModel(
           PayloadAnswerModel.fromJson(payloadJson),
+          date,
+          ttl,
         );
       case Type.time_out:
         return SocketMessageTimeOutModel(
           PayloadTimeOutModel.fromJson(payloadJson),
+          date,
+          ttl,
         );
       case Type.player_is_active:
         return SocketMessagePlayerIsActiveModel(
           PayloadEmptyModel.fromJson(payloadJson),
+          date,
+          ttl,
         );
       default:
         throw FormatException('unsupported type $type for SocketMessage');
