1c992b4c0e92b5c990cd0b7209fa7e4e39afe7ef
Michael Yemets <mgyemets@gmail.com>
2022-02-25 09:46:17 +0200

    tmp

---
+++ lib/data/db_adapter/rss_item_adapter.dart
@@ -0,0 +1,43 @@
+import 'package:hive/hive.dart';
+import 'package:satorio/data/db_adapter/adapter_type_ids.dart';
+import 'package:webfeed/domain/rss_content.dart';
+import 'package:webfeed/webfeed.dart';
+
+class RssItemAdapter extends TypeAdapter<RssItem> {
+  @override
+  int get typeId => RssItemTypeId;
+
+  @override
+  RssItem read(BinaryReader reader) {
+    final title = reader.readString();
+    final description = reader.readString();
+    final link = reader.readString();
+    final guid = reader.readString();
+    final pubDate = DateTime.tryParse(reader.readString());
+    final contentValue = reader.readString();
+    final contentImages = reader.readStringList();
+
+    return const RssItem(
+      title: title,
+      description: description,
+      link: link,
+      guid: guid,
+      pubDate: pubDate,
+      content: RssContent(
+        contentValue,
+        contentImages,
+      ),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, RssItem rssItem) {
+    writer.writeString(rssItem.title ?? '');
+    writer.writeString(rssItem.description ?? '');
+    writer.writeString(rssItem.link ?? '');
+    writer.writeString(rssItem.guid ?? '');
+    writer.writeString(rssItem.pubDate?.toIso8601String() ?? '');
+    writer.writeString(rssItem.content?.value ?? '');
+    writer.writeStringList(rssItem.content?.images.toList() ?? []);
+  }
+}
