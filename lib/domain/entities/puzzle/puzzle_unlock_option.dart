b9dc655822091b87c9f3d2ca4f04738b056a7598
Michael Yemets <mgyemets@gmail.com>
2022-04-08 14:28:46 +0300

    improve puzzle options & puzzle finish texts

--- lib/domain/entities/puzzle/puzzle_unlock_option.dart
+++ lib/domain/entities/puzzle/puzzle_unlock_option.dart
@@ -2,10 +2,12 @@ class PuzzleUnlockOption {
   final String id;
   final double amount;
   final int steps;
+  final bool isLocked;
 
   const PuzzleUnlockOption(
     this.id,
     this.amount,
     this.steps,
+    this.isLocked,
   );
 }
