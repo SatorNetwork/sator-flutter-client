88687b872b888ebb18778a051372881394406a2a
MichaelBoostyLabs <84083729+MichaelBoostyLabs@users.noreply.github.com>
2022-04-29 12:41:29 +0300

    Asset clean (#229)
    * remove unused assets
    
    * simplify profile structure
    
    * simplify profile structure
    
    Co-authored-by: Michael Yemets <mgyemets@gmail.com>

--- lib/controller/activity_controller.dart
+++
@@ -1,6 +0,0 @@
-import 'package:get/get.dart';
-import 'package:satorio/controller/mixin/non_working_feature_mixin.dart';
-
-class ActivityController extends GetxController with NonWorkingFeatureMixin {
-
-}
