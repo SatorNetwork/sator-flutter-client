d854baca93a2fb6caf1bd1c9df343eeb553075d2
mbortsov <mike.bortsov@gmail.com>
2022-05-13 12:18:07 +0300

    in app on android fixed

--- lib/controller/show_detail_with_episodes_controller.dart
+++ lib/controller/show_detail_with_episodes_controller.dart
@@ -6,6 +6,7 @@ import 'package:satorio/controller/nft_list_controller.dart';
 import 'package:satorio/controller/show_episode_realm_controller.dart';
 import 'package:satorio/domain/entities/nft_filter_type.dart';
 import 'package:satorio/domain/entities/nft_item.dart';
+import 'package:satorio/domain/entities/nft_order_type.dart';
 import 'package:satorio/domain/entities/show.dart';
 import 'package:satorio/domain/entities/show_detail.dart';
 import 'package:satorio/domain/entities/show_episode.dart';
@@ -61,7 +62,8 @@ class ShowDetailWithEpisodesController extends GetxController
   }
 
   void _loadNftItems() {
-    _satorioRepository.nftsFiltered(showIds: [showRx.value.id]).then(
+    _satorioRepository.nftsFiltered(
+        orderType: NftOrderOnSaleType.onSale, showIds: [showRx.value.id]).then(
       (List<NftItem> nftItems) {
         nftItemsRx.value = nftItems;
       },
