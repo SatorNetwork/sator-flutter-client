ec1c3ca5b03ba9d905a9ef80ef3bc4d7eb5c3a97
Michael Yemets <mgyemets@gmail.com>
2022-05-16 11:37:59 +0300

    tmp

--- lib/data/datasource/impl/solana_data_source_impl.dart
+++ lib/data/datasource/impl/solana_data_source_impl.dart
@@ -9,30 +9,87 @@ class SolanaDataSourceImpl implements SolanaDataSource {
   static const String _dev = 'FBDfbe7CFXHHNzDpNBYf4Evcg5GKrThYNjk4wP2xwjwA';
   static const String _prod = '2HeykdKjzHKGm2LKHw8pDYwjKPiFEoXAz74dirhUgQvq';
 
-  late final RpcClient _rpcClient;
+  late final SolanaClient _solanaClient;
 
   SolanaDataSourceImpl() {
-    print('SolanaDataSourceImpl');
-    _rpcClient = RpcClient(_urlDevNet, timeout: const Duration(seconds: 30));
+    _solanaClientTmp();
+    _rpcClientTmp();
+  }
 
-    _rpcClient.getAccountInfo(_dev).then((value) => print('SOLANA getAccountInfo $value'));
+  void _rpcClientTmp() {
+    print('SOLANA _rpcClientTmp');
 
-    _rpcClient.getBalance(_dev).then((value) => print('SOLANA getBalance $value'));
+    final pubKey = _dev;
 
-    _rpcClient.getProgramAccounts(_dev, encoding: Encoding.base58).then((value) => print('SOLANA getProgramAccounts $value'));
+    _solanaClient.rpcClient
+        .getAccountInfo(pubKey)
+        .then((value) => print('SOLANA getAccountInfo $value'))
+        .onError(
+          (Exception error, stackTrace) =>
+              print('SOLANA getAccountInfo ${error.toString()}'),
+        );
 
-    _rpcClient.getSignaturesForAddress(_dev).then((value) => print('SOLANA getSignaturesForAddress $value'));
+    _solanaClient.rpcClient
+        .getBalance(pubKey)
+        .then((value) => print('SOLANA getBalance $value'))
+        .onError(
+          (Exception error, stackTrace) =>
+              print('SOLANA getBalance ${error.toString()}'),
+        );
 
-    _rpcClient.getStakeActivation(_dev).then((value) => print('SOLANA getStakeActivation $value'));
+    _solanaClient.rpcClient
+        .getProgramAccounts(pubKey, encoding: Encoding.base58)
+        .then((value) => print('SOLANA getProgramAccounts $value'))
+        .onError(
+          (Exception error, stackTrace) =>
+              print('SOLANA getProgramAccounts ${error.toString()}'),
+        );
 
-    _rpcClient.getTokenAccountBalance(_dev).then((value) => print('SOLANA getTokenAccountBalance $value'));
+    _solanaClient.rpcClient
+        .getSignaturesForAddress(pubKey)
+        .then((List<TransactionSignatureInformation> value) =>
+            print('SOLANA getSignaturesForAddress ${value.length}'))
+        .onError(
+          (Exception error, stackTrace) =>
+              print('SOLANA getSignaturesForAddress ${error.toString()}'),
+        );
 
-    _rpcClient.getTokenLargestAccounts(_dev).then((value) => print('SOLANA getTokenLargestAccounts $value'));
+    _solanaClient.rpcClient
+        .getStakeActivation(pubKey)
+        .then((value) => print('SOLANA getStakeActivation $value'))
+        .onError(
+          (Exception error, stackTrace) =>
+              print('SOLANA getStakeActivation ${error.toString()}'),
+        );
+
+    _solanaClient.rpcClient
+        .getTokenAccountBalance(pubKey)
+        .then((value) => print('SOLANA getTokenAccountBalance $value'))
+        .onError(
+          (Exception error, stackTrace) =>
+              print('SOLANA getTokenAccountBalance ${error.toString()}'),
+        );
+
+    // _rpcClient
+    //     .getTokenLargestAccounts(pubKey)
+    //     .then((value) => print('SOLANA getTokenLargestAccounts $value'))
+    //     .onError(
+    //       (Exception error, stackTrace) =>
+    //           print('SOLANA getTokenLargestAccounts ${error.toString()}'),
+    //     );
+  }
+
+  void _solanaClientTmp() {
+    print('SOLANA _solanaClientTmp');
+    _solanaClient = SolanaClient(
+      rpcUrl: Uri.parse(_urlDevNet),
+      websocketUrl: Uri.parse(_urlDevNet),
+      timeout: Duration(seconds: 30),
+    );
   }
 
   @override
   Future<void> balanceSOL() {
-
     return Future.value();
   }
 
@@ -50,5 +107,4 @@ class SolanaDataSourceImpl implements SolanaDataSource {
   Future<void> nftList() {
     return Future.value();
   }
-
 }
