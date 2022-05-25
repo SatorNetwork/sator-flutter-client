4eaf7428a4169f5577c8183b8887117944a1ffb1
Vlad Garkusha <cc.setwo@gmail.com>
2022-05-25 12:11:33 +0300

    unity integration update.

--- lib/unity/unity_view_page.dart
+++ lib/unity/unity_view_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_unity_widget/flutter_unity_widget.dart';
 import 'package:get/get.dart';
 import 'package:satorio/data/datasource/auth_data_source.dart';
+import 'package:satorio/data/datasource/firebase_data_source.dart';
 
 class UnityViewPage extends StatefulWidget {
   UnityViewPage() : super();
@@ -18,21 +19,21 @@ class _UnityViewPageState extends State<UnityViewPage> {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: () async {
-        await _unityWidgetController.pause();
-        Navigator.of(context).pop();
-        return false;
-      }, child:
-         Scaffold(
+        onWillPop: () async {
+          await _unityWidgetController.pause();
+          Navigator.of(context).pop();
+          return false;
+        }, child:
+    Scaffold(
       appBar: AppBar(
         title: const Text('Sator Universe'),
       ),
       body: UnityWidget(
-        onUnityCreated: onUnityCreated,
-        onUnityMessage: onUnityMessage,
-        onUnitySceneLoaded: onUnitySceneLoaded,
-        fullscreen: false,
-        borderRadius: BorderRadius.zero
+          onUnityCreated: onUnityCreated,
+          onUnityMessage: onUnityMessage,
+          onUnitySceneLoaded: onUnitySceneLoaded,
+          fullscreen: false,
+          borderRadius: BorderRadius.zero
       ),
     ));
   }
@@ -68,11 +69,13 @@ class _UnityViewPageState extends State<UnityViewPage> {
     );
 
     var tkn = await Get.find<AuthDataSource>().getAuthToken() as String;
+    var url = await Get.find<FirebaseDataSource>().apiBaseUrl() as String;
+    print('Sending token to Unity!: ${tkn}');
 
     _unityWidgetController.postMessage(
         'GameStarter',
         'Initialize',
-        tkn.substring(tkn.length - 3)
+        tkn + '!' + url
     );
   }
 }
